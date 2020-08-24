a2789d0d8612b4f55d89bfdb6a1d633a1f1aae47
IshanJais <70131013+IshanJais@users.noreply.github.com>
2020-08-25 00:46:51 +0530

    Taking input number from user and printing

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout << "Enter the number you want to print: ";
+	cin >> number;
+	cout << "You entered the number: " << number;
+	return 0;
+}
