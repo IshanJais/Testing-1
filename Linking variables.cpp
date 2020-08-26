83dc2a462e9fa6fc8bb88093858e1012320de43e
Ishan Jais <70131013+IshanJais@users.noreply.github.com>
2020-08-26 13:58:45 +0530

    Add files via upload

---
+++ Linking variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+void Log(const char* message);
+int main()
+{
+	std::cout << multiply(10, 20) << std::end1;
+	std::cin.get();
+}
+int multiply(int a, int b)
+{
+	Log("Multiply");
+	return a * b;
+}
